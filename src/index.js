47d09cd1da8a743c3b27003997a7f1b6f312b9f3
Michael Townsend <michael@doublespeakgames.com>
2019-12-24 11:59:57 -0500

    Engagement tracking

--- src/index.js
+++ src/index.js
@@ -15,8 +15,17 @@ const scroll = new SmoothScroll('a[href*="#"]', {
   easing: 'easeInOutCubic'
 });
 
+let tracked = false;
 window.addEventListener('scroll', () => {
   const scrollPercent = Math.min(1, window.scrollY / (window.innerHeight * 0.8));
   document.getElementById('arrow').style.opacity = 1 - scrollPercent;
   document.getElementById('content').style.opacity = scrollPercent;
+
+  if (scrollPercent == 1 && !tracked) {
+    tracked = true;
+    gtag('event', 'view_item', {
+      'event_category': 'engagement',
+      'event_label': 'content'
+    });
+  }
 });
