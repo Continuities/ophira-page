9674c6a988fcdd23ff3a6a59047dd94cc0071371
Michael Townsend <michael@doublespeakgames.com>
2019-12-29 13:10:45 -0500

    Background

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ let tracked = false;
 window.addEventListener('scroll', () => {
   const scrollPercent = Math.min(1, window.scrollY / (window.innerHeight * 0.8));
   document.getElementById('arrow').style.opacity = 1 - scrollPercent;
-  document.getElementById('content').style.opacity = scrollPercent;
+  document.getElementById('content-text').style.opacity = scrollPercent;
 
   if (scrollPercent == 1 && !tracked) {
     tracked = true;
